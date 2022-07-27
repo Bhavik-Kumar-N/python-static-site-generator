3d2aa6fdf8a347ec993e2491a05805ccd9aa8eaa
Bhavik Kumar N <bhavik.n@stl.tech>
2022-07-27 14:39:39 +0530

    The Content Class (2)

--- ssg/content.py
+++ ssg/content.py
@@ -3,13 +3,13 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimiter="^(?:-|\+){3}\s*$"
+    __delimiter=r"^(?:-|\+){3}\s*$"
     __regex=re.compile(__delimiter,re.MULTILINE)
     
     @classmethod
     def load(self, cls, string):
         _, fm, content=cls.__regex.split(string,2)
-        load(fm, Loader=FullLoader)
+        metadata=load(fm, Loader=FullLoader)
         return cls(self.metadata, content)
     
     def __init__(self, metadata, content):
